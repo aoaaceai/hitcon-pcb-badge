9adc94acde990b078b642dc4d4b9b101058a0762
aoaaceai <aoaaceai@csie.ntu.edu.tw>
2025-05-03 22:02:48 +0800

    Untested implementation of the hash service

--- fw/Core/Hitcon/Service/HashService.h
+++ fw/Core/Hitcon/Service/HashService.h
@@ -1,11 +1,47 @@
 #ifndef HASH_SERVICE_H
 #define HASH_SERVICE_H
 
+#include <Logic/keccak.h>
+#include <Service/Sched/Scheduler.h>
 #include <Util/callback.h>
 #include <stddef.h>
 #include <stdint.h>
 
 namespace hitcon {
+
+namespace hash {
+
+constexpr size_t SHA3_BIT_SIZE = 256;
+
+namespace internal {
+
+struct HashStatus {
+  size_t progress;
+  int round;
+  enum state { update, finalize, done } state;
+
+  HashStatus();
+  void Init();
+  void NewState(enum state state);
+};
+
+struct ServiceContext {
+  uint8_t const *message;
+  size_t len;
+  callback_t callback;
+  void *callbackArg1;
+
+  ServiceContext();
+  void Init(uint8_t const *message, size_t len, callback_t callback,
+            void *callbackArg1);
+};
+
+}  // namespace internal
+
+struct HashResult {
+  uint8_t *digest;
+  size_t size;
+};
 
 class HashService {
  public:
@@ -19,10 +55,28 @@ class HashService {
   // callback will be a uint8_t pointer to the hash result, it'll only be valid
   // during the callback.
   bool StartHash(uint8_t const *message, size_t len, callback_t callback,
-                 void *callback_arg1);
+                 void *callbackArg1);
+
+  HashService();
+
+ private:
+  service::sched::PeriodicTask hashTask;
+
+  internal::ServiceContext serviceContext;
+  internal::HashStatus status;
+  sha3_context sha3Context;
+  HashResult result;
+
+  void doHash(void *unused);
+
+  void doHashUpdate();
+  void doHashFinalize();
+  void doHashDone();
 };
 
 extern HashService g_hash_service;
+
+}  // namespace hash
 
 }  // namespace hitcon
 
